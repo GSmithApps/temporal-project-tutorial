15268459a2d9bc6b54fa3f40aea135646b102903
Grant Smith <gsmith5@instructors.2u.com>
2024-08-15 23:31:12 -0500

    increased timeout

--- workflow.py
+++ workflow.py
@@ -6,6 +6,8 @@ from temporalio import workflow
 with workflow.unsafe.imports_passed_through():
     from activities import get_address_from_user, get_api_key_from_user, get_lat_long, QueryParams
 
+_TIMEOUT_5_MINS=5*60
+
 @workflow.defn
 class GeoCode:
 
@@ -13,20 +15,19 @@ class GeoCode:
     async def run(self) -> list:
 
         api_key_from_user =  await workflow.execute_activity(
-            get_api_key_from_user, start_to_close_timeout=timedelta(seconds=5)
+            get_api_key_from_user, start_to_close_timeout=timedelta(seconds=_TIMEOUT_5_MINS)
         )
         
         address_from_user =  await workflow.execute_activity(
-            get_address_from_user, start_to_close_timeout=timedelta(seconds=5)
+            get_address_from_user, start_to_close_timeout=timedelta(seconds=_TIMEOUT_5_MINS)
         )
 
         query_params = QueryParams(api_key=api_key_from_user, address=address_from_user)
 
         lat_long = await workflow.execute_activity(
-            get_lat_long, query_params, start_to_close_timeout=timedelta(seconds=5)
+            get_lat_long, query_params, start_to_close_timeout=timedelta(seconds=_TIMEOUT_5_MINS)
         )
 
         return lat_long
-
 
 # @@@SNIPEND
